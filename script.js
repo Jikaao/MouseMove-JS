96c81b8ce7dfb5e6f20b5974a00dd387f00f7fda
Jordan B <Jikao55@outlook.fr>
2023-10-15 18:02:42 +0200

    correction mousemove pagex/y

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const cursorBorder = document.getElementById("cursor-border");
 
 window.addEventListener("mousemove", (e) => {
   mousemove.forEach((mouse) => {
-    mouse.style.left = e.x + "px"; // .x ou .y permet de faire suivre le cursor sur les axes Y et X en même temps que la souris.
-    mouse.style.top = e.y + "px";
+    mouse.style.left = e.pageX + "px"; // .x ou .y permet de faire suivre le cursor sur les axes Y et X en même temps que la souris.
+    mouse.style.top = e.pageY + "px";
   });
 });
 // ---------------------Pour aggrandir la bordure du cursor au clique de la souris----------------------------------------------------
